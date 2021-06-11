d915a0f8ff426307fe71e5b04aa71095bec0cfdd
snadax <glgg3321@gmail.com>
2021-06-12 01:31:59 +0800

    Update signin.py

--- signin.py
+++ signin.py
@@ -26,6 +26,20 @@ inajax = '&inajax=1'
 
 def run(form_data):
     s = requests.Session()
+    s.headers = {
+    "authority":"www.hao4k.cn" ,
+    "cache-control":"max-age=0" ,
+    "sec-ch-ua":"\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"" ,
+    "sec-ch-ua-mobile":"?0" ,
+    "upgrade-insecure-requests":"1" ,
+    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36" ,
+    "accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" ,
+    "sec-fetch-site":"none" ,
+    "sec-fetch-mode":"navigate" ,
+    "sec-fetch-user":"?1" ,
+    "sec-fetch-dest":"document" ,
+    "accept-language":"zh-CN,zh;q=0.9" 
+    }
     user_resp = s.get(user_url)
     login_text = re.findall('action="(.*?)"', user_resp.text)
     for loginhash in login_text:
